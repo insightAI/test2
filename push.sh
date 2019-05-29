eabe18d42185d57aa605cfc7f1f4b98d034ac031
insightAI <clickyeah@yeah.net>
2019-05-30 07:14:00 +0800

    first commit

---
+++ push.sh
@@ -0,0 +1,9 @@
+git init
+git add .
+git add $1
+git commit -m 'first commit'
+git remote add origin https://github.com/insightAI/test2.git
+git pull --rebase origin master
+git push -u origin master
+git status
+
